36adec3b1e9f916ff37aa07ef7de92dd74dd6b94
Mt <matjaz.zupanic1@gmail.com>
2022-04-04 16:59:26 +0200

    popravki diferencialne enačbe - test.py

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ w.addItem(g)
 ## compute surface vertex data
 rows = 100
 cols = 100
-x=np.linspace(-10, 10, cols)
-y=np.linspace(-10, 10, rows)
+X=np.linspace(-10, 10, cols)
+Y=np.linspace(-10, 10, rows)
 ## create a surface plot, tell it to use the 'heightColor' shader
 ## since this does not require normal vectors to render (thus we
 ## can set computeNormals=False to save time when the mesh updates)
-p4 = gl.GLSurfacePlotItem(x = x, y = y, shader='heightColor', computeNormals=False, smooth=False)
+p4 = gl.GLSurfacePlotItem(x = X, y = Y, shader='heightColor', computeNormals=False, smooth=False)
 p4.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
 w.addItem(p4)
 
@@ -42,29 +42,25 @@ def randomMatrix():
             arr.append(10 * random())
         matrika.append(arr)
     return np.array(matrika)
-def dqdx_matrix(q_matrix):  #poracuna odvode matrike q_matrix po dx, tudi za 2. , n. odvode
-    dqdx = []
+
+def d2qdv2_fun(u_v1, u_v, u_v_1, dv):
+    return (u_v1 - 2 * u_v + u_v_1) / (dv ** 2)
+def d2qdv2_matrix(q):  #poracuna odvode matrike q_matrix po dy
+    d2qdv2 = []
     for y in range(1, rows - 1):
-        dqdx_y = []
+        dqdv_y = []
         for x in range(1, cols - 1):
-            dqdx_y.append(((q_0[y][x - 1] - q_0[y][x]) / dx + (q_0[y][x + 1] - q_0[y][x]) / dx) / 2)
-        dqdx.append(dqdx_y)
-    return dqdx
-def dqdy_matrix(q_matrix):  #poracuna odvode matrike q_matrix po dy
-    dqdy = []
-    for x in range(1, rows - 1):
-        dqdy_x = []
-        for y in range(1, cols - 1):
-            dqdy_x.append(((q_0[y - 1][x] - q_0[y][x]) / dy + (q_0[y+1][x] - q_0[y][x]) / dy) / 2)
-        dqdy.append(dqdy_x)
-    return dqdy
+            dqdv_y.append(d2qdv2_fun(q[y][x+1],q[y][x],q[y][x-1],dx) + d2qdv2_fun(q[y+1][x],q[y][x],q[y-1][x],dy))
+
+        d2qdv2.append(dqdv_y)
+    return d2qdv2
 def D_laplatz_q_dt(D,laplatz,dt):
     dq=[]
     for y in range(len(laplatz)):
-        dq_x = []
+        dq_y = []
         for x in range(len(laplatz[y])):
-            dq_x.append(D*laplatz[y][x]*dt)
-        dq.append(dq_x)
+            dq_y.append(D*laplatz[y][x]*dt)
+        dq.append(dq_y)
     return dq
 def starting_q(cols, rows):
     q = []
@@ -72,7 +68,7 @@ def starting_q(cols, rows):
         q_x = []
         for x in range(cols):
             if(cols/3 < x < 2*cols/3 and rows/3 < y < 2*rows/3):
-                q_x.append(5)
+                q_x.append(0.5)
             else:
                 q_x.append(0)
         q.append(q_x)
@@ -81,28 +77,28 @@ def starting_q(cols, rows):
 def q_dq_sum(q,dq):
     qsum = []
     for y in range(len(q)):
-        qsum_x=[]
+        qsum_y=[]
         for x in range(len(q[y])):
             try:
-                qsum_x.append(q[y][x]+dq[y][x])
+                if(0<q[y][x]<1):
+                    qsum_y.append(q[y][x]+dq[y][x])
+                else:
+                    qsum_y.append(q[y][x])
             except:
-                qsum_x.append(q[y][x])
-        qsum.append(qsum_x)
+                qsum_y.append(q[y][x])
+        qsum.append(qsum_y)
     return qsum
 iteration=0
-dt = 0.1 #Delta čas
+
 q_0 = starting_q(cols, rows) #začetno stanje koncentracije
 dx = 20 / cols
 dy = 20 / rows
-D = -0.01
+D = 0.1
+dt = 0.001
 def update():
     global iteration,q_0
-    dqdx = dqdx_matrix(q_0) #prvi odvod po x
-    dqdy = dqdy_matrix(q_0) #prvi odvod po y
-    d2qdx2 = dqdx_matrix(dqdx)  # drugi odvod po x
-    d2qdy2 = dqdy_matrix(dqdy)  # drugi odvod po y
 
-    laplatz=d2qdx2+d2qdy2
+    laplatz = d2qdv2_matrix(q_0)
     dq = D_laplatz_q_dt(D,laplatz,dt)
     q_1 =  q_dq_sum(q_0,dq)
 
--- test.py
+++ test.py
@@ -1,2 +1,102 @@
-import pyqtgraph.examples
-pyqtgraph.examples.run()
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from pyqtgraph.Qt import QtCore
+
+
+## Create a GL View widget to display data
+app = pg.mkQApp("GLSurfacePlot Example")
+w = gl.GLViewWidget()
+w.show()
+w.setWindowTitle('Difuzija')
+w.setCameraPosition(distance=50)
+
+g = gl.GLGridItem()
+g.scale(1,1,1)
+g.setDepthValue(10)  # draw grid after surfaces since they may be translucent
+w.addItem(g)
+rows = 50
+cols = 50
+X=np.linspace(-10, 10, cols)
+Y=np.linspace(-10, 10, rows)
+
+p4 = gl.GLSurfacePlotItem(x=X,y=Y,shader='heightColor', computeNormals=False, smooth=False)
+p4.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
+w.addItem(p4)
+
+
+print("2D heat equation solver")
+
+plate_length = 50
+max_iter_time = 10000
+
+alpha = 2
+delta_x = 1
+
+delta_t = (delta_x ** 2)/(4 * alpha)
+gamma = (alpha * delta_t) / (delta_x ** 2)
+
+# Initialize solution: the grid of u(k, i, j)
+u = np.empty((max_iter_time, plate_length, plate_length))
+
+# Initial condition everywhere inside the grid
+u_initial = 0
+
+# Boundary conditions
+u_top = 10.0
+u_left = 0.0
+u_bottom = 0.0
+u_right = 0.0
+
+# Set the initial condition
+u.fill(u_initial)
+
+# Set the boundary conditions
+u[:, (plate_length-1):, :] = u_top
+u[:, :, :1] = u_left
+u[:, :1, 1:] = u_bottom
+u[:, :, (plate_length-1):] = u_right
+
+def calculate():
+    global p4,u
+    #for k in range(0, max_iter_time-1, 1):
+    for i in range(1, plate_length-1, delta_x):
+        for j in range(1, plate_length-1, delta_x):
+            u[1, i, j] = gamma * (u[0][i+1][j] + u[0][i-1][j] + u[0][i][j+1] + u[0][i][j-1] - 4*u[0][i][j]) + u[0][i][j]
+    print("test ")
+    p4.setData(z=u[1])
+    u[0]=u[1]
+
+
+def plotheatmap(u_k, k):
+    # Clear the current plot figure
+    plt.clf()
+
+    plt.title(f"Temperature at t = {k*delta_t:.3f} unit time")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    # This is to plot u_k (u at time-step k)
+    plt.pcolormesh(u_k, cmap=plt.cm.jet, vmin=0, vmax=100)
+    plt.colorbar()
+
+    return plt
+
+# Do the calculation here
+#u = calculate(u)
+timer = QtCore.QTimer()
+timer.timeout.connect(calculate)
+timer.start(30)
+
+if __name__ == '__main__':
+    pg.exec()
+#def animate(k):
+    #plotheatmap(u[k], k)
+
+#anim = animation.FuncAnimation(plt.figure(), animate, interval=1, frames=max_iter_time, repeat=False)
+#anim.save("heat_equation_solution.gif")
+
+#print("Done!")
